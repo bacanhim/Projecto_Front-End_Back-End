2ef3166fd4d64e3b4b833b6752fdb3ad275de273
fernand0nunes <nandooo.nunes@gmail.com>
2019-06-07 02:34:31 +0100

    07/06/19 02:34
    assim nao fode o que fizeste com o backend

---
+++ PASTAtestefront/vuefrontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <router-view/>
+    <v-content>
+      <Signup/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Signup from './components/Signup.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Signup
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
