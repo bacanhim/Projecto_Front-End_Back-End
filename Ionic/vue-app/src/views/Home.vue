fab7f1bfc9c92fec5daeb58322cac4ecdea6aeda
Hélder Bacanhim <43825339+phn0m@users.noreply.github.com>
2019-06-19 23:03:39 +0100

    bn

--- Ionic/vue-app/src/views/Home.vue
+++ Ionic/vue-app/src/views/Home.vue
@@ -75,8 +75,37 @@
         </ion-row>
       </ion-card>
     </ion-content>
+<ion-footer>
+      <ion-toolbar>
+        <ion-tabs>
+          <ion-tab tab="home">Home Content</ion-tab>
+          <ion-tab tab="settings">Settings Content</ion-tab>
 
-   
+          <ion-tab-bar slot="bottom">
+            <ion-tab-button tab="/">
+              <ion-label>Inicio</ion-label>
+              <ion-icon name="home"></ion-icon>
+              <ion-badge></ion-badge>
+            </ion-tab-button>
+
+            <ion-tab-button tab="Amigos">
+              <ion-label>Amigos</ion-label>
+              <ion-icon name="contacts"></ion-icon>
+            </ion-tab-button>
+
+            <ion-tab-button tab="Explorar">
+              <ion-label>Explorar</ion-label>
+              <ion-icon name="search"></ion-icon>
+            </ion-tab-button>
+
+            <ion-tab-button tab="definicoes">
+              <ion-label>Definições</ion-label>
+              <ion-icon name="settings"></ion-icon>
+            </ion-tab-button>
+          </ion-tab-bar>
+        </ion-tabs>
+      </ion-toolbar>
+    </ion-footer>
   </ion-app>
 </template>
 
