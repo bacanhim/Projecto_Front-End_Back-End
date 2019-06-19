7fb3c97ee303ce07c2ea3ab2e0d3992d77dad6dc
fernand0nunes <nandooo.nunes@gmail.com>
2019-06-19 20:29:37 +0100

    12

--- Ionic/vue-app/src/views/Perfil.vue
+++ Ionic/vue-app/src/views/Perfil.vue
@@ -16,9 +16,76 @@
         </ion-row>
       </ion-toolbar>
     </ion-header>
+    <ion-content>
+      <ion-card color="light">
+        <ion-list>
+          <ion-item>
+            <ion-avatar item-start>
+              <img
+                src="https://pbs.twimg.com/profile_images/971557554177347584/NNowGsXX_400x400.jpg"
+              >
+            </ion-avatar>
+            <h2>Gil Veloza</h2>
+          </ion-item>
+          <img src="http://tiny.cc/z8di8y">
+          <ion-card-content color="light">
+            <p>Gosto de RedBull!!!!!!!!!</p>
+          </ion-card-content>
+        </ion-list>
+        <ion-row>
+          <ion-col>
+            <ion-button icon-start color="dark" small>
+              <ion-icon name="thumbs-up"></ion-icon>
+              <div>12 likes</div>
+            </ion-button>
+          </ion-col>
+          <ion-col>
+            <ion-button icon-start color="dark" small>
+              <ion-icon name="text"></ion-icon>
+              <div>4 Comments</div>
+            </ion-button>
+          </ion-col>
+          <ion-col align-self-center text-center>
+            <ion-note>11h ago</ion-note>
+          </ion-col>
+        </ion-row>
+
+        <ion-item>
+          <ion-avatar item-start>
+            <img src="http://tiny.cc/f5di8y">
+          </ion-avatar>
+          <h2>Fernando Nunes</h2>
+          <p>1 hora atras</p>
+        </ion-item>
+
+        <img src="http://tiny.cc/z8di8y">
+
+        <ion-card-content>
+          <p>Gosto de RedBull!!!!!!!!!</p>
+        </ion-card-content>
+
+        <ion-row>
+          <ion-col>
+            <ion-button icon-start color="dark" small>
+              <ion-icon name="thumbs-up"></ion-icon>
+              <div>12 likes</div>
+            </ion-button>
+          </ion-col>
+          <ion-col>
+            <ion-button icon-start color="dark" small>
+              <ion-icon name="text"></ion-icon>
+              <div>4 Comments</div>
+            </ion-button>
+          </ion-col>
+          <ion-col align-self-center text-center>
+            <ion-note>11h ago</ion-note>
+          </ion-col>
+        </ion-row>
+      </ion-card>
+    </ion-content>
     <ion-footer>
       <ion-toolbar>
-       <ion-tabs>
+        <ion-tabs>
           <ion-tab tab="home">Home Content</ion-tab>
           <ion-tab tab="settings">Settings Content</ion-tab>
 
